0ea7664814ac5fc7e10a9204c13aa688b61e3afd
AmeliaMaier <amelia.maier.jikili@gmail.com>
2018-02-28 20:47:36 -0700

    add exponential methods to scipy_adapter

--- data_analytics/scipy_adapter.py
+++ data_analytics/scipy_adapter.py
@@ -47,3 +47,43 @@ class ScipyAdapter():
             Creates a generator that will create random numbers that generally follow the poisson distribution.
         '''
         return scs.poisson.rvs(mean, size=num_ran_vars)
+
+    def exponential_probability_density_function(x_values):
+        '''
+            INPUTS: x_values (the bins at the bottom of the graph)
+            RETURNS: the formula generator (can be passed into         matplotlib
+
+            In probability theory and statistics, the exponential distribution (also known as negative exponential distribution) is the probability distribution that describes the time between events in a Poisson point process, i.e., a process in which events occur continuously and independently at a constant average rate.
+        '''
+        return scs.expon.pdf(x_values)
+
+    def exponential_cumulative_distribution_function():
+        '''
+            INPUTS: x_values (the bins at the bottom of the graph)
+                     mean (mu, witch hat symble, lambda)
+            RETURNS: the formula generator (can be passed into         matplotlib
+
+            In probability theory and statistics, the exponential distribution (also known as negative exponential distribution) is the probability distribution that describes the time between events in a Poisson point process, i.e., a process in which events occur continuously and independently at a constant average rate.
+        '''
+        return scs.expon.cdf(x_values)
+
+    def exponential_stats(moments_str):
+        '''
+            INPUT: moments_str (mvsk or shorter)
+            RETURNS: tuple of requested variables (mean, var, skew, kurt)
+
+            mean: 'm' (will be the same as the lambda that was provided)
+            variance: 'v'
+            skew: 's'
+            kurt: 'k'
+        '''
+        return scs.expon.stats(moments=moments_str)
+
+    def exponential_random_variables(num_ran_vars):
+        '''
+            INPUTS: num_ran_vars (the number of random varables you want generated)
+            RETURNS: the random number generator
+
+            Creates a generator that will create random numbers that generally follow the poisson distribution.
+        '''
+        return scs.expon.rvs(size=num_ran_vars)
